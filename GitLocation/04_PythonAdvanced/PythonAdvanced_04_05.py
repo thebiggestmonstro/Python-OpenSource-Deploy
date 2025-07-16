8afede40d82757e6bb3cc59ed6a0632216902d83
thebiggestmonstro <monstroowner@gmail.com>
2025-07-16 22:01:59 +0900

    GitHub Package Deploy

---
+++ GitLocation/04_PythonAdvanced/PythonAdvanced_04_05.py
@@ -0,0 +1,6 @@
+
+# GitHub를 통해 배포하는 경우 PyPI를 사용할 때와 다르게 빌드 결과물은 필요없다
+
+# 1) 패키지 폴더에 git을 설치한다
+# 2) PyPI 형태의 패키지 구조를 GitHub Repository에 Push
+# GitHub에 패보된 패키지 설치 : pip install git+https://repository-url
